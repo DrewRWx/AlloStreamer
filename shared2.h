4ca7ecf8d41ed224d87884c4f8a0c35f248b20dd
Drew Waranis <drew@waran.is>
2013-12-20 14:03:11 -0800

    One location to change image dimensions.

--- shared2.h
+++ shared2.h
@@ -3,5 +3,10 @@
 
 const std::string log_dir = "./Logs/";
 
+// const int image_width = 1280;
+// const int image_height = 720;
+const int image_width = 1024;
+const int image_height = 576;
+
 #endif
 
